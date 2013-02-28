5c1b8f24b503a65e0ea136efbb0f54e7fe8c63d5
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2013-02-27 22:58:23 -0800

    Make search more like sublime
    Once isearch start, Enter finds the 
    next instance & Shift-Enter finds the previous instance.
    
    Also, double clicking on a word should highlight all other
    matching words.

--- dot-emacs.el
+++ dot-emacs.el
@@ -150,9 +150,24 @@
   (package-initialize)
   (load-theme 'solarized-dark t))
 
-(define-key isearch-mode-map (kbd "C-m")
+;; Some Sublime Text-isms:
+
+(define-key isearch-mode-map (kbd "<return>")
   'isearch-repeat-forward)
-(define-key isearch-mode-map (kbd "<C-S-return>")
+(define-key isearch-mode-map (kbd "<S-return>")
   'isearch-repeat-backward)
+
+(require 'advice)
+
+(defadvice mouse-drag-region (after my-sublime-like-mouse-select (start-event))
+  (when (= (event-click-count start-event) 2)
+    (let ((isearch-word t)
+            (isearch-forward t)
+	  (string (buffer-substring-no-properties (mark) (point))))
+      (save-excursion
+	(call-interactively 'isearch-forward)
+	(isearch-yank-string string)))))
+
+(ad-activate 'mouse-drag-region)
 
 (message "")
