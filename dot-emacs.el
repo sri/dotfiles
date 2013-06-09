e53450ee3f6d1678a09c51f597cf3bb909fcdbfc
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2013-06-09 09:46:55 -0700

    magit handle when there are no sections

--- dot-emacs.el
+++ dot-emacs.el
@@ -280,7 +280,8 @@
         ((eq major-mode 'magit-status-mode)
          (let* ((current (magit-current-section))
                 (parent (magit-section-parent current)))
-           (if (eq (magit-section-title parent) 'stashes)
+           (if (and parent
+                    (eq (magit-section-title parent) 'stashes))
                (magit-show-item-or-scroll-up)
              (magit-toggle-section))))
         ((memq major-mode '(magit-wazzup-mode
