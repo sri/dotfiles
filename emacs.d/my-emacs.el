1ec6881758aeea453fee8586446e69669acb4075
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2013-10-12 18:29:51 -0700

    Refactor my-load function.

--- emacs.d/my-emacs.el
+++ emacs.d/my-emacs.el
@@ -1,17 +1,19 @@
-(defun my-load (path)
-  (let* ((path (expand-file-name path)) ;binds w/ default-directory
-         (src (concat path ".el"))
-         (cpl (concat path ".elc")))
-    (if (file-newer-than-file-p src cpl)
-        (byte-compile-file src))
-    (load cpl nil nil t)))
+(defun my-load (filename-no-ext)
+  "Compiles the file, if necessary, and then loads it.
+Filename-no-ext should be the full name of the file with
+the extension."
+  (let ((source (concat filename-no-ext ".el"))
+        (compiled (concat filename-no-ext ".elc")))
+    (when (file-newer-than-file-p source compiled)
+      (byte-compile-file source))
+    (load compiled nil t t)))
 
-(let ((my-files '("my-env" "my-fns" "my-keys" "my-dired" "my-shell"
-                  "my-packages" "my-bm" "my-magit"
+(let ((my-files '("my-env" "my-fns" "my-keys" "my-dired"
+                  "my-shell" "my-packages" "my-bm" "my-magit"
                   "my-sublime" "my-modeline"))
-      (default-directory (file-name-directory load-file-name)))
+      (load-directory (file-name-directory load-file-name)))
   (dolist (f my-files)
-    (my-load f)))
+    (my-load (expand-file-name f load-directory))))
 
 (let ((private (expand-file-name "~/.emacs.private")))
   (when (file-exists-p (concat private ".el"))
