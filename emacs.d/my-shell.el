20a4c18529603015419034f16d69f485dcb89b12
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2013-11-20 22:59:06 -0800

    Shell improvements.
    Allow Esc-. to handle quotes correctly.
    Switch shell buffers easily.
    Also, disable the header line since I'm not using it that much.

--- emacs.d/my-shell.el
+++ emacs.d/my-shell.el
@@ -80,47 +80,77 @@
              (setq string ""))))
     (comint-simple-send proc string)))
 
-(defvar my-shell-bash-esc-dot-map
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "M-.") 'my-shell-bash-esc-dot)
-    map))
-
 (defvar my-shell-bash-esc-dot-counter 0)
-(make-variable-buffer-local 'my-shell-bash-esc-dot-counter)
+(defvar my-shell-bash-esc-dot-last-insertion nil)
 
 (defun my-shell-bash-esc-dot ()
-  "Output the last word for the previous command."
+  "Same as Esc-. in bash; insert previous commands last word."
   (interactive)
   (let* ((continue (eq last-command 'my-shell-bash-esc-dot))
          (count (if continue (1+ my-shell-bash-esc-dot-counter) 0))
          (cmd (comint-previous-input-string count))
-         ;; Fix me -- respect quotes, etc
-         (last (car (last (split-string cmd " " t)))))
+         (last (if (string-match "\\([`'\"]\\)[^`'\"]+?\\1\\s-*$" cmd)
+                   (match-string 0 cmd)
+                 (car (last (split-string cmd " " t))))))
     (setq my-shell-bash-esc-dot-counter count)
     (when last
-      ;; Delete last insertion.
       (when continue
-        (save-excursion
-          (end-of-line)
-          (re-search-backward "[^ ]" (point-at-bol) nil)
-          (if (search-backward " " (point-at-bol) t)
-              (forward-char 1)
-            (beginning-of-line))
-          (delete-region (point) (point-at-eol))))
-      (insert last))
-    (when continue
-      (set-temporary-overlay-map my-shell-bash-esc-dot-map t))))
+        (delete-region (point)
+                       (save-excursion
+                         (search-backward my-shell-bash-esc-dot-last-insertion
+                                          (point-at-bol)))))
+      (setq my-shell-bash-esc-dot-last-insertion last)
+      (insert last))))
+
+(defun my-shell (&optional arg)
+  "Create a new shell (with prefix arg) or switch to a shell buffer."
+  (interactive "P")
+  (if arg
+      (shell (generate-new-buffer-name "*shell*"))
+    (let (shells others)
+      (dolist (buf (buffer-list))
+        (when (eq (with-current-buffer buf major-mode) 'shell-mode)
+          (if (string-match "^[*]shell[*]" (buffer-name buf))
+              (push buf shells)
+            (push buf others))))
+      ;; Sort the shells name "*shell*", "*shell*<1>" by their names.
+      (setq shells (sort shells (lambda (x y)
+                                  (string-lessp (buffer-name x)
+                                                (buffer-name y)))))
+      ;; Sort the shells not named starting with "*shell*" by their
+      ;; creation time.
+      (setq others (sort others (lambda (x y)
+                                  (< (with-current-buffer x
+                                       my-shell-mode-created-at)
+                                     (with-current-buffer y
+                                       my-shell-mode-created-at)))))
+      (cond ((and (null shells) (null others))
+             (shell))
+            ((eq major-mode 'shell-mode)
+             (let ((cur (current-buffer)))
+               (switch-to-buffer (if (string-match "^[*]shell[*]" (buffer-name))
+                                     (or (cadr (memq cur shells))
+                                         (car others)
+                                         (car shells))
+                                   (or (cadr (memq cur others))
+                                       (car shells)
+                                       (car others))))))
+            (t (switch-to-buffer (or (car shells)
+                                     (car others))))))))
+
+(defvar my-shell-mode-created-at nil)
+(make-variable-buffer-local 'my-shell-mode-created-at)
 
 (add-hook 'shell-mode-hook
           (lambda ()
-            (my-setup-shell-header-line)
+            ;; (my-setup-shell-header-line)
+            (setq my-shell-mode-created-at (float-time))
             (linum-mode -1)
             (setq line-number-mode nil
                   column-number-mode nil)
             (setq comint-input-sender
                   'my-shell-input-sender)
             (toggle-truncate-lines 1)
-            ;(buffer-disable-undo)
             (dirtrack-mode)
             (define-key shell-mode-map (kbd "C-<up>")
               'comint-previous-prompt)
