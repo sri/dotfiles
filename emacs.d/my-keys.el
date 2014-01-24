873a50c461dac7d2d463fa6999152ead7b8bd8be
Sriram Thaiyar <sthaiyar@on-site.com>
2014-01-24 08:54:08 -0800

    Some frame commands. C-S-n: make new frame C-S-w: delete current frame

--- emacs.d/my-keys.el
+++ emacs.d/my-keys.el
@@ -34,13 +34,14 @@
 (global-set-key (kbd "C-(") 'kmacro-end-macro)
 (global-set-key (kbd "<C-return>") 'my-quick-hotkey)
 
-(global-set-key (kbd "S-C-f") 'my-find-in-directory)
 (global-set-key (kbd "S-C-d") 'my-duplicate-line-or-region)
-(global-set-key (kbd "S-C-j") 'isearch-backward)
+(global-set-key (kbd "S-C-f") 'my-find-in-directory)
 (global-set-key (kbd "S-C-j") 'join-line)
 (global-set-key (kbd "S-C-k") 'my-kill-whole-line)
+(global-set-key (kbd "S-C-n") 'make-frame-command)
 (global-set-key (kbd "S-C-o") 'jump-to-register)
 (global-set-key (kbd "S-C-r") 'query-replace-regexp)
+(global-set-key (kbd "S-C-w") 'delete-frame)
 
 (global-set-key (kbd "<M-down>") 'scroll-up)
 (global-set-key (kbd "<M-up>") 'scroll-down)
