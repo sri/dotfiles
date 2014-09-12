02f8682307db5c4d829e5044ba643a1d2e2393ad
Sriram Thaiyar <sthaiyar@on-site.com>
2014-09-11 21:32:13 -0700

    Sublime-like C-d (but when in isearch)

--- emacs.d/my-isearch.el
+++ emacs.d/my-isearch.el
@@ -23,9 +23,18 @@
       (isearch-search)
       (isearch-update))))
 
+(defun my-isearch-yank-whole-word ()
+  (interactive)
+  (isearch-yank-internal
+   (lambda ()
+     (skip-chars-forward "^ \n\t\r")
+     (point))))
+
+
 (define-key isearch-mode-map (kbd "C-v")
   'my-isearch-goto-next-non-visible-match)
 
 (define-key isearch-mode-map (kbd "C-o") 'isearch-occur)
 (define-key isearch-mode-map (kbd "C-k") 'isearch-query-replace)
 (define-key isearch-mode-map (kbd "C-K") 'isearch-query-replace-regexp)
+(define-key isearch-mode-map (kbd "C-d") 'my-isearch-yank-whole-word)
