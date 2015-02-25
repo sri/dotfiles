7e435a976c492d7cd834df1baee9f75618b25f89
Sriram Thaiyar <sthaiyar@on-site.com>
2015-02-24 18:21:23 -0800

    Better key bindings when in console (binds to Alt-<key>)

--- emacs/emacs.d/my-bm.el
+++ emacs/emacs.d/my-bm.el
@@ -5,21 +5,8 @@
           'bm-highlight-only-fringe
         'bm-highlight-only-line))
 
-(defvar my-bm-next-keymap
-  (let ((map (make-sparse-keymap)))
-    (define-key map (kbd "n") 'bm-next)
-    map))
-
-(defun my-bm-next ()
-  (interactive)
-  (message "Hit 'n' to goto the next bookmark")
-  (bm-next)
-  (set-temporary-overlay-map my-bm-next-keymap t))
-
 (global-set-key (kbd "<C-f2>") 'bm-toggle)
 (global-set-key (kbd "C-S-SPC") 'bm-toggle)
-(global-set-key (kbd "C-c m") 'bm-toggle)
-(global-set-key (kbd "C-c n") 'my-bm-next)
 (global-set-key (kbd "<f2>") 'bm-next)
 (global-set-key (kbd "<S-f2>") 'bm-previous)
 (global-set-key (kbd "<C-S-f2>") 'bm-remove-all-current-buffer)
--- emacs/emacs.d/my-keys.el
+++ emacs/emacs.d/my-keys.el
@@ -20,6 +20,9 @@
 (global-set-key (kbd "C-y") 'my-yank)
 (global-set-key (kbd "C-z") 'undo)
 
+(global-set-key (kbd "<home>") 'beginning-of-buffer)
+(global-set-key (kbd "<end>") 'end-of-buffer)
+
 (global-set-key (kbd "C-c TAB") 'yas/expand)
 (global-set-key (kbd "C-c \\") 'align-regexp)
 (global-set-key (kbd "C-c a") 'org-agenda)
@@ -53,6 +56,41 @@
 (global-set-key (kbd "C-\"") 'my-emacs-lisp-eval)
 (global-set-key (kbd "<C-return>") 'kill-ring-save)
 ;; (global-set-key (kbd "C-\\") ')
+
+;; I like to use Emacs in a console (via iTerm)
+(unless window-system
+  (global-set-key (kbd "º") 'delete-window)               ; 0
+  (global-set-key (kbd "¡") 'delete-other-windows)        ; 1
+  (global-set-key (kbd "™") 'split-window-vertically)     ; 2
+  (global-set-key (kbd "£") 'split-window-horizontally)   ; 3
+  (global-set-key (kbd "≤") 'beginning-of-buffer)         ; ,
+  (global-set-key (kbd "≥") 'end-of-buffer)               ; .
+  (global-set-key (kbd "…") 'eval-expression)             ; ;
+  (global-set-key (kbd "«") 'my-emacs-lisp-eval)          ; \
+  (global-set-key (kbd "æ") 'eval-last-sexp)
+  (global-set-key (kbd "ESC <up>") 'scroll-down)
+  (global-set-key (kbd "ESC <down>") 'scroll-up)
+  (global-set-key (kbd "∂") 'my-dired)                    ; d
+  (global-set-key (kbd "¿") 'my-comment-line-or-region)   ; S-/
+  (global-set-key (kbd " ") 'bm-toggle)                   ; SPC
+  (global-set-key (kbd "∫") 'bm-next)                     ; b
+  (global-set-key (kbd "∆") 'other-window)                ; j
+
+  (global-set-key (kbd "Î") 'my-duplicate-line-or-region) ; S-d
+  (global-set-key (kbd "") 'my-kill-whole-line)          ; S-k
+  (global-set-key (kbd "¬") 'mc/edit-lines)               ; l
+
+  (global-set-key (kbd "µ") 'magit-status)                ; m
+  (global-set-key (kbd "ƒ") 'my-isearch-forward)          ; f
+
+  (global-set-key (kbd "ß") 'save-buffer)                 ; s
+  (global-set-key (kbd "Ω") 'undo)
+  ;; (global-set-key (kbd "") ')
+  ;; (global-set-key (kbd "") ')
+  ;; (global-set-key (kbd "") ')
+  ;; (global-set-key (kbd "") ')
+)
+
 
 (global-set-key (kbd "S-C-b") 'bm-next)
 (global-set-key (kbd "S-C-c") 'my-duplicate-line-or-region)
