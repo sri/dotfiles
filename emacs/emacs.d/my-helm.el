335e938c97a53487bbb285f29de79d1a4e6d6ebc
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2015-05-29 22:43:25 -0700

    Use <up> and <down> for element selection.
    That is the standard binding for Helm.

--- emacs/emacs.d/my-helm.el
+++ emacs/emacs.d/my-helm.el
@@ -1,9 +1,6 @@
 (require 'helm-config)
 (helm-mode 1)
 
-(define-key helm-map (kbd "<up>") 'previous-history-element)
-(define-key helm-map (kbd "<down>") 'next-history-element)
-
 (setq helm-ff-skip-boring-files t)
 ;; TODO: these don't work. Looks like having the '.' and '..' on top
 ;; of the list is by design.
