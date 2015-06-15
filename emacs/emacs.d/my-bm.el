23bdf43f7df3cf8db4b2b8fc6c788a3bfcd378c4
Sriram Thaiyar <sthaiyar@on-site.com>
2015-06-15 14:51:24 -0700

    New keys for bookmarking

--- emacs/emacs.d/my-bm.el
+++ emacs/emacs.d/my-bm.el
@@ -4,12 +4,3 @@
       (if window-system
           'bm-highlight-only-fringe
         'bm-highlight-only-line))
-
-(global-set-key (kbd "<C-f2>") 'bm-toggle)
-(global-set-key (kbd "C-S-SPC") 'bm-toggle)
-(global-set-key (kbd "<f2>") 'bm-next)
-(global-set-key (kbd "<S-f2>") 'bm-previous)
-(global-set-key (kbd "<C-S-f2>") 'bm-remove-all-current-buffer)
-(global-set-key (kbd "<left-fringe> <mouse-5>") 'bm-next-mouse)
-(global-set-key (kbd "<left-fringe> <mouse-4>") 'bm-previous-mouse)
-(global-set-key (kbd "<left-fringe> <mouse-1>") 'bm-toggle-mouse)
