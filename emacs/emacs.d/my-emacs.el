07a1f50c14f7654454490f5547b6465e24449d32
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2015-07-12 16:56:32 -0700

    Move package init to dotemacs

--- emacs/emacs.d/my-emacs.el
+++ emacs/emacs.d/my-emacs.el
@@ -1,54 +1,27 @@
-(defvar my-packages
-  '(
-    autopair
-    bm
-    browse-kill-ring
-    coffee-mode
-    elisp-slime-nav
-    flycheck
-    go-mode
-    swiper
-    macrostep
-    magit
-    org
-    projectile
-    rainbow-mode
-    ruby-end
-    smart-mode-line
-    solarized-theme
-    visual-regexp
-    yasnippet
-    ))
-
 (require 'dash)
 
 (defun my-load-all ()
   (interactive)
   (let ((default-directory "~/my/dotfiles/emacs/emacs.d")
 	(my-files
-         '(
-           "my-fns"
-           "my-env"
-           "my-keys"
-           "my-win"
-           "my-view"
-           "my-sublime"
-           "my-shell"
-           "my-occur"
-           "my-isearch"
-           "my-help"
-           "my-dired"
-           ))
+         (list "my-fns"
+               "my-env"
+               "my-keys"
+               "my-win"
+               "my-view"
+               "my-sublime"
+               "my-shell"
+               "my-occur"
+               "my-isearch"
+               "my-help"
+               "my-dired"
+               ))
         (my-non-packages
          ;; Files that aren't on MELPA or any other
          ;; package archive.
          (directory-files "~/.emacs.d/third-party" 'full "\\.el$" t))
         (my-private "~/.emacs.private.el"))
 
-    ;; Install missing packages
-    (-when-let (missing (-remove #'package-installed-p my-packages))
-      (package-refresh-contents)
-      (mapc 'package-install missing))
     (mapc 'my-load my-non-packages)
     ;; Load all my files: main reason to load this
     ;; before package customizations: they might
