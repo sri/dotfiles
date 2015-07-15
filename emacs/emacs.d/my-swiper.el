148874b5b23a2ca29c7a8e7d9bdfd0441ef2fc5a
Sriram Thaiyar <sthaiyar@on-site.com>
2015-07-14 22:56:35 -0700

    Better keys for ivy completions

--- emacs/emacs.d/my-swiper.el
+++ emacs/emacs.d/my-swiper.el
@@ -1,3 +1,11 @@
 (ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
+
+(setq ivy-extra-directories nil)
+
+(define-key ivy-minibuffer-map (kbd "C-j") 'ivy-immediate-done)
+(define-key ivy-minibuffer-map (kbd "<left>") 'ivy-backward-delete-char)
+(define-key ivy-minibuffer-map (kbd "<right>") 'ivy-partial-or-done)
+(define-key ivy-minibuffer-map (kbd "C-a") 'ivy-beginning-of-buffer)
+(define-key ivy-minibuffer-map (kbd "C-e") 'ivy-end-of-buffer)
+
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
