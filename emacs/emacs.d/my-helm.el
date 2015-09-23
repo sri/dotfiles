d7a7787b3a94b6558db9ea4484dfb2d3c55c2760
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2015-09-23 00:56:40 -0700

    More helm stuff

--- emacs/emacs.d/my-helm.el
+++ emacs/emacs.d/my-helm.el
@@ -5,7 +5,7 @@
 (define-key minibuffer-local-map (kbd "C-c C-l") 'helm-minibuffer-history)
 
 (setq helm-M-x-fuzzy-match t)
-
+(setq helm-buffers-fuzzy-matching t)
 ;; (setq helm-ff-skip-boring-files t)
 ;; ;; TODO: these don't work. Looks like having the '.' and '..' on top
 ;; ;; of the list is by design.
