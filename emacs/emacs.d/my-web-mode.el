98cb4d6ecb5e9a3a9c09eca5270d6842534be17f
Sriram Thaiyar <sthaiyar@on-site.com>
2016-02-24 16:14:23 -0800

    Configure web-mode

---
+++ emacs/emacs.d/my-web-mode.el
@@ -0,0 +1,13 @@
+(require 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string= web-mode-content-type "javascript")
+              (web-mode-set-content-type "jsx"))
+            (setq web-mode-markup-indent-offset 2)
+            (setq web-mode-css-indent-offset 2)
+            (setq web-mode-sql-indent-offset 2)
+            (setq web-mode-code-indent-offset 2)))
