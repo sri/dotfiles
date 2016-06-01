0dec4c32ac4739a8207a4b2431e66760ef0b500e
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-05-31 21:57:48 -0700

    Don't show menu bar if not using window system

--- emacs/emacs.d/my-env.el
+++ emacs/emacs.d/my-env.el
@@ -81,7 +81,9 @@ Fundamental mode."
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (winner-mode 1)
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (and (fboundp 'menu-bar-mode)
+         (null window-system))
+    (menu-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (line-number-mode t)
