c700e133af12eb7b2f90db9542896c975d43983f
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-06-28 22:01:25 -0700

    Add imenu

--- emacs/emacs.d/my-keys.el
+++ emacs/emacs.d/my-keys.el
@@ -29,6 +29,7 @@
 (global-set-key (kbd "C-c g") 'helm-grep-do-git-grep)
 (global-set-key (kbd "C-c i") 'my-change-inside-pair)
 (global-set-key (kbd "C-c l") 'toggle-truncate-lines)
+(global-set-key (kbd "C-c m") 'imenu)
 (global-set-key (kbd "C-c o") 'helm-occur)
 (global-set-key (kbd "C-c s") 'sort-lines)
 (global-set-key (kbd "C-c v") 'helm-ls-git-ls)
