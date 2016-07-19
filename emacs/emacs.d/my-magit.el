fd1ceded79816254d02d2497b7ddb79f2c7d2337
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-07-19 00:49:38 -0700

    This var is too distracting

--- emacs/emacs.d/my-magit.el
+++ emacs/emacs.d/my-magit.el
@@ -1,7 +1,7 @@
 (require 'magit)
 
 (setq magit-save-repository-buffers 'dontask)
-(setq magit-diff-refine-hunk t)
+(setq magit-diff-refine-hunk nil)
 
 (set-face-attribute 'magit-diff-added-highlight nil :foreground "#22aa22")
 
