01ae75bef564884ff69d8e151a6435418141d812
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-09-01 23:11:13 -0700

    Up the font-size

--- emacs/emacs.d/my-win.el
+++ emacs/emacs.d/my-win.el
@@ -26,7 +26,7 @@
     (add-to-list 'default-frame-alist (cons 'width 80))
     (add-to-list 'default-frame-alist (cons 'top top))
     (add-to-list 'default-frame-alist (cons 'left left))
-    (add-to-list 'default-frame-alist '(font . "Monaco 14")))
+    (add-to-list 'default-frame-alist '(font . "Monaco 18")))
 
   (set-frame-parameter nil 'alpha '(100 100))
 
