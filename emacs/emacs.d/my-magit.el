35a8d34165fc628948bfe30849fb0a7cc5568965
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-09-25 22:03:01 -0700

    Use digits to invoke the -all functions.

--- emacs/emacs.d/my-magit.el
+++ emacs/emacs.d/my-magit.el
@@ -6,3 +6,10 @@
 (set-face-attribute 'magit-diff-added-highlight nil :foreground "#22aa22")
 
 (add-hook 'magit-log-edit-mode-hook 'turn-on-auto-fill)
+
+(add-hook 'magit-mode-hook
+          (lambda ()
+            (define-key magit-mode-map (kbd "1") 'magit-section-show-level-1-all)
+            (define-key magit-mode-map (kbd "2") 'magit-section-show-level-2-all)
+            (define-key magit-mode-map (kbd "3") 'magit-section-show-level-3-all)
+            (define-key magit-mode-map (kbd "4") 'magit-section-show-level-4-all)))
