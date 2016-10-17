218cc139274d46065be55f553878e08d5ed3fa5b
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-10-17 15:05:11 -0700

    Use full path

--- emacs/init.el
+++ emacs/init.el
@@ -1,4 +1,4 @@
-(setq custom-file (expand-file-name "custom.el"))
+(setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
 (load custom-file)
 
 (require 'cl)
@@ -10,8 +10,8 @@
 (package-initialize)
 
 (require 'org)
-(let ((compiled (expand-file-name "emacs.elc"))
-      (tangled (expand-file-name "emacs.org")))
+(let ((compiled (expand-file-name "~/.emacs.d/emacs.elc"))
+      (tangled (expand-file-name "~/.emacs.d/emacs.org")))
   (if (file-newer-than-file-p tangled compiled)
-      (org-babel-load-file (expand-file-name "emacs.org") 'compile)
+      (org-babel-load-file tangled 'compile)
     (load compiled nil 'nosuffix)))
