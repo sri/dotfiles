6de99a44ea0ba1fc977b3db64b16b2e3dc45753a
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-10-19 13:49:00 -0700

    Add which-key

--- emacs/emacs.d/my-diminish.el
+++ emacs/emacs.d/my-diminish.el
@@ -1,5 +1,8 @@
 (require 'diminish)
 
+;; To see which variable needs to be added below:
+;; (progn (describe-variable 'minor-mode-alist) (switch-to-buffer-other-window "*Help*"))
+
 (let ((modes
        '(yas-minor-mode
          anzu-mode
@@ -8,6 +11,8 @@
          elisp-slime-nav-mode
          org-indent-mode
          dired-omit-mode
+         ruby-end-mode
          region-bindings-mode
+         which-key-mode
          auto-revert-mode)))
   (mapc 'diminish modes))
