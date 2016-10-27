d9d12a1ba05f482fabb9c0c651e776ae93bf76a5
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-10-27 13:07:30 -0700

    Refactor for readability

--- emacs/emacs.d/my-emacs.el
+++ emacs/emacs.d/my-emacs.el
@@ -2,48 +2,38 @@
 
 (defun my-load-all ()
   (interactive)
-  (let ((default-directory "~/my/dotfiles/emacs/emacs.d")
-        (my-files
-         (list "my-fns"
-               "my-env"
-               "my-keys"
-               "my-view"
-               "my-sublime"
-               "my-shell"
-               "my-occur"
-               "my-isearch"
-               "my-help"
-               "my-dired"
-               "my-org-activity-log"
-               "my-update-dot-emacs"
-               ))
-        (packages-custom-order
-         ;; Load order of packages customized by me.
-         ;; Gets loaded in this order after
-         ;; other package custom files.
-         '(diminish))
-        (my-non-packages
-         ;; Files that aren't on MELPA or any other
-         ;; package archive.
-         (directory-files "~/.emacs.d/third-party" 'full "\\.el$" t))
-        (my-private "~/.emacs.private.el"))
+  (let ((default-directory "~/my/dotfiles/emacs/emacs.d"))
 
     (my-load (if window-system "my-gui" "my-terminal"))
 
-    (mapc 'my-load my-non-packages)
-    ;; Load all my files: main reason to load this
-    ;; before package customizations: they might
-    ;; depend on functions in my-fns.el (and other
-    ;; dependencies like that).
-    (mapc 'my-load my-files)
+    ;; Files that aren't on MELPA or any other
+    ;; package archive.
+    (mapc 'my-load
+          (directory-files "~/.emacs.d/third-party" 'full "\\.el$" t))
 
-    (dolist (pkg package-selected-packages)
-      (unless (memq pkg packages-custom-order)
-        (my-load (format "my-%s.el" pkg) 'ignore-if-missing)))
-    (dolist (pkg packages-custom-order)
-      (my-load (format "my-%s.el" pkg) 'ignore-if-missing))
+    (mapc 'my-load
+          '(
+            "my-fns"
+            "my-env"
+            "my-keys"
+            "my-view"
+            "my-sublime"
+            "my-shell"
+            "my-occur"
+            "my-isearch"
+            "my-help"
+            "my-dired"
+            "my-org-activity-log"
+            "my-update-dot-emacs"
+            ))
 
-    (my-load my-private 'ignore-if-missing)))
+    (mapc (lambda (pkg)
+            (my-load (format "my-%s" pkg) 'ignore-if-missing))
+          (-remove-item 'diminish
+                        package-selected-packages))
+
+    (my-load "my-diminish")
+    (my-load "~/.emacs.private.el" 'ignore-if-missing)))
 
 (my-load-all)
 
