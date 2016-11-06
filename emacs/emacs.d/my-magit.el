637a3d09d6d56157b8ab625840c20480ebca2fab
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2016-11-05 22:07:21 -0700

    More magit hacks

--- emacs/emacs.d/my-magit.el
+++ emacs/emacs.d/my-magit.el
@@ -1,7 +1,12 @@
 (require 'magit)
 
 (setq magit-save-repository-buffers 'dontask)
-(setq magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1)
+(setq magit-commit-show-diff nil)
+(setq magit-display-buffer-function
+      'magit-display-buffer-same-window-except-diff-v1)
+
+(remove-hook 'magit-status-sections-hook
+             'magit-insert-stashes)
 
 (set-face-attribute 'magit-diff-added-highlight nil :foreground "#22aa22")
 
@@ -9,17 +14,18 @@
 
 (defun my-magit-diff-toggle-refine-hunk ()
   (interactive)
-  (setq magit-diff-refine-hunk
-        (cond ((eq magit-diff-refine-hunk t) 'all)
-              ((eq magit-diff-refine-hunk 'all) nil)
-              (t t)))
-  (message "Word-diff: %s"
-           (cond ((eq magit-diff-refine-hunk t) "current hunk")
-                 ((eq magit-diff-refine-hunk 'all) "all")
-                 (t "off"))))
+  (let* ((vals '((t . "current hunk")
+                 (all . "all")
+                 (nil . "off")))
+         (next (or (cadr (cl-member magit-diff-refine-hunk vals
+                                    :key #'car))
+                   (car vals))))
+    (setq magit-diff-refine-hunk (car next))
+    (message "Word-diff: %s" (cdr next)))
 
 (add-hook 'magit-mode-hook
           (lambda ()
+            (define-key magit-mode-map (kbd "C-c C-s") 'magit-stash-list)
             (define-key magit-mode-map (kbd "C-c C-w") 'my-magit-diff-toggle-refine-hunk)
             (define-key magit-mode-map (kbd "1") 'magit-section-show-level-1-all)
             (define-key magit-mode-map (kbd "2") 'magit-section-show-level-2-all)
