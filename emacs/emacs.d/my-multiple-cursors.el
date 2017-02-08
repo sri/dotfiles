e7afe015a055c8236c692168296f4c5eefc8a0bb
Sriram Thaiyar <sthaiyar@on-site.com>
2017-02-08 01:01:09 -0800

    Fix typo

--- emacs/emacs.d/my-multiple-cursors.el
+++ emacs/emacs.d/my-multiple-cursors.el
@@ -2,6 +2,6 @@
 (require 'region-bindings-mode)
 
 (bind-keys :map region-bindings-mode-map
-           ("a" . c/mark-all-like-this)
-           ("n" . c/mark-next-like-this)
+           ("a" . mc/mark-all-like-this)
+           ("n" . mc/mark-next-like-this)
            ("l" . mc/edit-lines))
