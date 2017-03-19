403d561473a6be0038910dacff6bba2e5d6b5543
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2017-03-18 21:59:46 -0700

    More key bindings when region is active

--- emacs/emacs.d/my-multiple-cursors.el
+++
@@ -1,7 +0,0 @@
-(require 'multiple-cursors)
-(require 'region-bindings-mode)
-
-(bind-keys :map region-bindings-mode-map
-           ("a" . mc/mark-all-like-this)
-           ("n" . mc/mark-next-like-this)
-           ("l" . mc/edit-lines))
--- emacs/emacs.d/my-region-bindings-mode.el
+++ emacs/emacs.d/my-region-bindings-mode.el
@@ -1,6 +1,18 @@
 (require 'region-bindings-mode)
+(require 'multiple-cursors)
 
 (region-bindings-mode-enable)
+
+(bind-keys :map region-bindings-mode-map
+           ("n" . next-line)
+           ("p" . previous-line)
+           ("e" . end-of-line)
+           ("a" . beginning-of-line)
+           ("w" . forward-word)
+           ("b" . backward-word)
+           ("a" . mc/mark-all-like-this)
+           ("n" . mc/mark-next-like-this)
+           ("l" . mc/edit-lines))
 
 (add-to-list 'region-bindings-mode-disable-predicates
              'minibufferp)
