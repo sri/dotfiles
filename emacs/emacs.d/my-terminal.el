3ecb1eed5c05ecdba5e5aa75601181457e062d28
Sriram Thaiyar <sthaiyar@on-site.com>
2017-04-04 01:31:58 -0700

    New theme for terminal emacs

--- emacs/emacs.d/my-terminal.el
+++ emacs/emacs.d/my-terminal.el
@@ -1,5 +1,5 @@
-(load-theme 'spacemacs-dark)
-(set-face-background 'lazy-highlight "#29422d")
-(face-spec-set 'hl-line '((t (:background "color-236" :inherit nil))))
+(load-theme 'ample-flat)
+;(set-face-background 'lazy-highlight "#29422d")
+;(face-spec-set 'hl-line '((t (:background "color-236" :inherit nil))))
 
 ;; (xterm-mouse-mode)
