4ebbbe349998b471b0a67845cff418fd95498364
Sriram Thaiyar <sthaiyar@on-site.com>
2017-05-25 04:18:35 -0700

    Improve terminal search highlighting

--- emacs/emacs.d/my-terminal.el
+++ emacs/emacs.d/my-terminal.el
@@ -1,4 +1,8 @@
 (load-theme 'ample-flat)
+
+(face-spec-set 'isearch '((t (:background "#22aa22" :foreground "black"))))
+(face-spec-set 'lazy-highlight '((t (:foreground "black" :background "green"))))
+
 ;(set-face-background 'lazy-highlight "#29422d")
 ;(face-spec-set 'hl-line '((t (:background "color-236" :inherit nil))))
 
