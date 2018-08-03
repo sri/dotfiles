59921a74dcafb2bbad2ec787b097b8292a09f1b7
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2018-08-03 00:14:21 -0700

    Refactor

--- emacs/emacs.d/my-vscode.el
+++ emacs/emacs.d/my-vscode.el
@@ -3,6 +3,12 @@
 (require 'windmove)
 
 (cl-defun my/shell-for-buffer (&optional create-new)
+  "Opens a shell for the current buffer in a window below it.
+If there is an existing shell, it is selected if it has the same
+directory or git repo as the current buffer. Invoking this
+command from the shell will close the shell buffer.
+
+With a prefix argument, generates a new shell for the current buffer."
   (interactive "P")
 
   (when (eq major-mode 'shell-mode)
@@ -12,29 +18,37 @@
   (cl-flet* ((git-root (buffer)
                (with-current-buffer buffer
                  (shell-command-to-string "git rev-parse --show-toplevel")))
-             (in-same-dir-or-repo? (buffer)
-               (and (with-current-buffer buffer (eq major-mode 'shell-mode))
-                    (and (let ((current (expand-file-name default-directory))
-                               (other (with-current-buffer buffer
-                                        (expand-file-name default-directory))))
-                           (or (string-prefix-p current other)
-                               (string-prefix-p other current)))
-                         (string= (git-root (current-buffer))
-                                  (git-root buffer)))))
+             (in-same-repo-or-dir? (buffer)
+               (and (with-current-buffer buffer
+                      (eq major-mode 'shell-mode))
+                    (let ((current (expand-file-name default-directory))
+                          (other (with-current-buffer buffer
+                                   (expand-file-name default-directory))))
+                      (or (string= current other)
+                          (and (or (string-prefix-p current other)
+                                   (string-prefix-p other current))
+                               (string= (git-root (current-buffer))
+                                        (git-root buffer)))))))
              (new-shell (&optional name)
+               (unless name
+                 (let ((shortened (abbreviate-file-name default-directory)))
+                   (setq name (format "*shell %s*"
+                                      (generate-new-buffer shortened)))))
                (save-window-excursion
-                 (shell (or name
-                            (generate-new-buffer (abbreviate-file-name default-directory)))))
+                 (shell name))
                name))
 
     (let ((win (window-in-direction 'below)))
-      (if (and win (in-same-dir-or-repo? (window-buffer win)))
+      (if (and win (in-same-repo-or-dir? (window-buffer win)))
           (windmove-down)
         (split-window-below)
         (windmove-down)
         (let ((new (if create-new
                        (new-shell)
-                     (if-let (buffer (cl-find-if #'in-same-dir-or-repo? (buffer-list)))
-                         (if (get-buffer-process buffer) buffer (new-shell (buffer-name buffer)))
+                     (if-let (buffer (cl-find-if #'in-same-repo-or-dir?
+                                                 (buffer-list)))
+                         (if (get-buffer-process buffer)
+                             buffer
+                           (new-shell (buffer-name buffer)))
                        (new-shell)))))
           (switch-to-buffer new))))))
