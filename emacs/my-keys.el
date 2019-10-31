bd324ed631df1f48e0d2ef9d63ece951599c4eea
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2019-10-31 11:57:35 -0700

    New key

--- emacs/my-keys.el
+++ emacs/my-keys.el
@@ -29,6 +29,7 @@
  ("s-2" . split-window-vertically)
  ("s-3" . split-window-horizontally)
  ("s-c" . my/copy-line-or-region)
+ ("s-x" . my/kill-line-or-region)
  ("s-g" . magit-status)
  ("s-," . beginning-of-buffer)
  ("s-o" . my/ffap-or-find-file)
