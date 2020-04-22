263d6164562d5c8aec631064bd6f971f1d958b29
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2020-04-21 21:11:48 -0700

    Add full Go support

--- emacs/my-go-mode.el
+++ emacs/my-go-mode.el
@@ -1,6 +1,32 @@
 (require 'go-mode)
+(require 'company)
+(require 'company-go)
 
-(add-to-list 'exec-path (expand-file-name "~/go/bin"))
+(let ((path "/usr/local/go/bin"))
+  (cond ((file-exists-p path)
+         (push path exec-path)
+         (setenv "PATH" (concat path ":" (getenv "PATH"))))
+        (t
+         (message "my-go-mode: path does not exist %s" path))))
 
-(setq gofmt-command "goimports")
+(add-hook 'go-mode-hook
+          (lambda ()
+            (setq tab-width 2)
+            (setq indent-tabs-mode 1)
+            (local-set-key (kbd "M-.") #'godef-jump)
+            (company-mode)
+            (set (make-local-variable 'company-backends) '(company-go))
+            '(go-eldoc-setup)
+
+            ))
+
+
+(let ((cmd "goimports"))
+  ;; Install: go get golang.org/x/tools/cmd/goimports
+  (if (executable-find cmd)
+      (setq gofmt-command "goimports")
+    (message "unable to find %s" cmd)))
 (add-hook 'before-save-hook 'gofmt-before-save)
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+(add-hook 'flycheck-mode-hook #'flycheck-golangci-lint-setup)
