c94504aab58fa93373fa2d9bcbdd95d485617238
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2021-12-21 21:46:32 -0700

    Python updates

--- pythonrc.py
+++ pythonrc.py
@@ -1,12 +1,31 @@
 import sys, os
 print("imported sys & os")
 
-from pprint import pprint as _pp
+# try:
+#     from rich import pretty as _pp
+#     _pp.install()
+#     _pp = print
+# except ImportError:
+#     from pprint import pprint as _pp
 
-def __displayhook(value):
-    if value is None:
-        return
-    __builtins__._ = value
-    _pp(value)
+# def __displayhook(value):
+#     if value is None:
+#         return
+#     __builtins__._ = value
+#     _pp(value)
 
-sys.displayhook = __displayhook
+# sys.displayhook = __displayhook
+
+try:
+    __IPYTHON__
+    running_under_ipython = True
+except NameError:
+    running_under_ipython = False
+
+if running_under_ipython:
+    from IPython.core.magic import register_line_magic
+
+    @register_line_magic("rev")
+    def lmagic(line):
+        "Rev line"
+        return line[::-1]
