3e13f38ded6160994e48489c1572e9a6916779ee
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2022-08-18 22:22:07 -0700

    refactor

--- bin/serve.go
+++ bin/serve.go
@@ -20,6 +20,25 @@ func listDir(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func serveFile(filename string, w http.ResponseWriter, req *http.Request) {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		fmt.Fprintf(w, "*** error reading file")
+		return
+	}
+	for _, f := range files {
+		if f.Name() == filename {
+			bytes, err := ioutil.ReadFile(f.Name())
+			if err != nil {
+				fmt.Fprintf(w, "*** error reading file")
+				return
+			}
+			w.Header().Set("Content-Type", http.DetectContentType(bytes))
+			w.Write(bytes)
+		}
+	}
+}
+
 func serve(w http.ResponseWriter, req *http.Request) {
 	// FIXME, eg: 127.0.0.1 - - [18/Aug/2022 21:23:06] "GET / HTTP/1.1" 200 -
 	fmt.Printf("[serving request %s %s]\n", req.Method, req.URL.Path)
@@ -28,27 +47,11 @@ func serve(w http.ResponseWriter, req *http.Request) {
 		listDir(w, req)
 	} else {
 		requestedFile := req.URL.Path[1:]
-
 		if strings.Contains(requestedFile, "..") ||
 			strings.ContainsAny(requestedFile, "\\/") {
 			fmt.Fprintf(w, "invalid file name")
 		} else {
-			files, err := ioutil.ReadDir(".")
-			if err != nil {
-				fmt.Fprintf(w, "*** error reading file")
-				return
-			}
-			for _, f := range files {
-				if f.Name() == requestedFile {
-					bytes, err := ioutil.ReadFile(f.Name())
-					if err != nil {
-						fmt.Fprintf(w, "*** error reading file")
-						return
-					}
-					w.Header().Set("Content-Type", http.DetectContentType(bytes))
-					w.Write(bytes)
-				}
-			}
+			serveFile(requestedFile, w, req)
 		}
 	}
 }
