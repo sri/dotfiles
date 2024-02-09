84aca7dfec58c8bad7359b5bc6be4da765599340
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2024-02-09 13:57:54 -0700

    fix all my ivy woes

--- emacs/my-ivy.el
+++ emacs/my-ivy.el
@@ -1,6 +1,22 @@
 (require 'ivy-avy)
+
 (setq ivy-re-builders-alist '((t . ivy--regex-ignore-order)))
 (setq ivy-wrap t)
+
+(defun my/ivy-toggle-mark ()
+  "Toggle mark for current candidate and move forwards."
+  (interactive)
+  (if (ivy--marked-p)
+      (ivy-unmark)
+    (ivy-mark)))
+(define-key ivy-minibuffer-map
+            (kbd "C-SPC")
+            'my/ivy-toggle-mark)
+
+(add-hook 'minibuffer-setup-hook (lambda () (override-global-mode -1)))
+(add-hook 'minibuffer-exit-hook (lambda () (override-global-mode 1)))
+
+
 (define-key ivy-minibuffer-map
             (kbd "S-<down>")
             'ivy-scroll-up-command)
