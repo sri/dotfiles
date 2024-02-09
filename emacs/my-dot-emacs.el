04cfdb019e1174192724d6c00a86f535e06e3343
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2024-02-09 10:43:32 -0700

    updates to ivy

--- emacs/my-dot-emacs.el
+++ emacs/my-dot-emacs.el
@@ -157,11 +157,42 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7533e1fc8345739ea0ace60330ebffdf9da46398490b4c36c7e48775e5621052" "bf798e9e8ff00d4bf2512597f36e5a135ce48e477ce88a0764cfb5d8104e8163" "c9ddf33b383e74dac7690255dd2c3dfa1961a8e8a1d20e401c6572febef61045" "f8067b7d0dbffb29a79e0843797efabdf5e1cf326639874d8b407e9b034136a4" "97965ccdac20cae22c5658c282544892959dc541af3e9ef8857dbf22eb70e82b" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "0e219d63550634bc5b0c214aced55eb9528640377daf486e13fb18a32bf39856" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" default))
+   '("7533e1fc8345739ea0ace60330ebffdf9da46398490b4c36c7e48775e5621052"
+     "bf798e9e8ff00d4bf2512597f36e5a135ce48e477ce88a0764cfb5d8104e8163"
+     "c9ddf33b383e74dac7690255dd2c3dfa1961a8e8a1d20e401c6572febef61045"
+     "f8067b7d0dbffb29a79e0843797efabdf5e1cf326639874d8b407e9b034136a4"
+     "97965ccdac20cae22c5658c282544892959dc541af3e9ef8857dbf22eb70e82b"
+     "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476"
+     "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279"
+     "0e219d63550634bc5b0c214aced55eb9528640377daf486e13fb18a32bf39856"
+     "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223"
+     "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e"
+     "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa"
+     "c5a044ba03d43a725bd79700087dea813abcb6beb6be08c7eb3303ed90782482"
+     "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e"
+     "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f"
+     default))
  '(org-modules
-   '(org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe org-rmail org-w3m org-velocity))
+   '(org-bbdb org-bibtex org-docview org-gnus org-info org-irc org-mhe
+              org-rmail org-w3m org-velocity))
  '(package-selected-packages
-   '(dired-sidebar diff-hl  ivy-rich helm-posframe ivy-posframe which-key rg expand-region xterm-color go-eldoc git-link company-go company flycheck-golangci-lint gotest exec-path-from-shell treemacs helm-projectile markdown-mode csv-mode swiper counsel ivy poet-theme gruvbox-theme dracula-theme kaolin-themes centaur-tabs typescript-mode protobuf-mode s rjsx-mode ample-theme leuven-theme neotree use-package yaml-mode diminish anzu region-bindings-mode hydra multiple-cursors undo-tree powerline spacemacs-theme zenburn-theme org yasnippet web-mode visual-regexp solarized-theme smart-mode-line ruby-end rainbow-mode projectile org-bullets magit macrostep helm helm-ls-git go-mode flycheck elisp-slime-nav bm ace-jump-mode))
+   '(ace-jump-mode ample-theme anzu bm centaur-tabs chatgpt-shell company
+                   company-go consult counsel csv-mode diff-hl
+                   diminish dired-sidebar dracula-theme
+                   elisp-slime-nav exec-path-from-shell expand-region
+                   flycheck flycheck-golangci-lint git-link go-eldoc
+                   go-mode gotest gruvbox-theme helm helm-ls-git
+                   helm-posframe helm-projectile hydra ivy ivy-avy
+                   ivy-posframe ivy-rich kaolin-themes leuven-theme
+                   macrostep magit markdown-mode multiple-cursors
+                   neotree org org-bullets poet-theme powerline
+                   projectile protobuf-mode rainbow-mode
+                   region-bindings-mode rg rjsx-mode ruby-end s
+                   smart-mode-line solarized-theme spacemacs-theme
+                   swiper treemacs typescript-mode undo-tree
+                   use-package vertico visual-regexp vterm web-mode
+                   which-key xterm-color yaml-mode yasnippet
+                   zenburn-theme zig-mode))
  '(typescript-indent-level 2))
 
 ;; Load the byte-compiled version of file.
--- emacs/my-ivy.el
+++ emacs/my-ivy.el
@@ -1,1 +1,12 @@
+(require 'ivy-avy)
 (setq ivy-re-builders-alist '((t . ivy--regex-ignore-order)))
+(setq ivy-wrap t)
+(define-key ivy-minibuffer-map
+            (kbd "S-<down>")
+            'ivy-scroll-up-command)
+(define-key ivy-minibuffer-map
+            (kbd "S-<up>")
+            'ivy-scroll-down-command)
+(define-key ivy-minibuffer-map
+            (kbd "C-i")
+            'ivy-avy)
