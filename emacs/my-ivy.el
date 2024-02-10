b334e6887e9cd5d02b1fbb51b86df085c4fa4b9a
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2024-02-09 17:32:34 -0700

    more ivy hacking

--- emacs/my-ivy.el
+++ emacs/my-ivy.el
@@ -1,7 +1,10 @@
+(require 'ivy)
 (require 'ivy-avy)
 
 (setq ivy-re-builders-alist '((t . ivy--regex-ignore-order)))
 (setq ivy-wrap t)
+(setq ivy-count-format "(%d/%d) ")
+(setq ivy-use-virtual-buffers t)
 
 (defun my/ivy-toggle-mark ()
   "Toggle mark for current candidate and move forwards."
@@ -9,20 +12,12 @@
   (if (ivy--marked-p)
       (ivy-unmark)
     (ivy-mark)))
-(define-key ivy-minibuffer-map
-            (kbd "C-SPC")
-            'my/ivy-toggle-mark)
 
 (add-hook 'minibuffer-setup-hook (lambda () (override-global-mode -1)))
 (add-hook 'minibuffer-exit-hook (lambda () (override-global-mode 1)))
 
-
-(define-key ivy-minibuffer-map
-            (kbd "S-<down>")
-            'ivy-scroll-up-command)
-(define-key ivy-minibuffer-map
-            (kbd "S-<up>")
-            'ivy-scroll-down-command)
-(define-key ivy-minibuffer-map
-            (kbd "C-i")
-            'ivy-avy)
+(bind-keys :map ivy-minibuffer-map
+           ("S-<down>" . ivy-scroll-up-command)
+           ("S-<up>" . ivy-scroll-down-command)
+           ("C-i" . ivy-avy)
+           ("C-SPC" . my/ivy-toggle-mark))
--- emacs/my-keys.el
+++ emacs/my-keys.el
@@ -7,16 +7,10 @@
 (require 'bind-key)
 
 ;; (global-set-key (kbd "C-s") 'swiper-isearch)
-;; (global-set-key (kbd "M-x") 'counsel-M-x)
-;; (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-;; (global-set-key (kbd "M-y") 'counsel-yank-pop)
-;; (global-set-key (kbd "<f1> f") 'counsel-describe-function)
-;; (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
 ;; (global-set-key (kbd "<f1> l") 'counsel-find-library)
 ;; (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
 ;; (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
 ;; (global-set-key (kbd "<f2> j") 'counsel-set-variable)
-;; (global-set-key (kbd "C-x b") 'ivy-switch-buffer)
 ;; (global-set-key (kbd "C-c v") 'ivy-push-view)
 ;; (global-set-key (kbd "C-c V") 'ivy-pop-view)
 
@@ -144,10 +138,12 @@
  ("C-h f" . counsel-describe-function)
 
  ("M-x" . my/meta-x)
- ("M-y" . helm-show-kill-ring)
+ ("M-y" . counsel-yank-pop)
 
  ("<home>" . beginning-of-buffer)
  ("<end>" . end-of-buffer)
+
+ ("C-x C-f" . counsel-find-file)
 
  ("C-x b" . list-buffers)
  ("C-x C-b" . dired-sidebar-toggle-with-current-directory)
