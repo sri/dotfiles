8d0097597838fb464dba234d39ece57ca5b971f6
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2024-07-07 09:53:18 -0700

    Fix yas stuff

--- emacs/my-yasnippet.el
+++ emacs/my-yasnippet.el
@@ -9,8 +9,13 @@
 (define-key yas-minor-mode-map [(tab)] nil)
 (define-key yas-minor-mode-map (kbd "TAB") nil)
 
-(defadvice yas--modes-to-activate (after my/yas-activate-global-mode ())
-  (let ((modes ad-return-value))
-    (setq ad-return-value (append modes (list 'global-mode)))))
+;; (defadvice yas--modes-to-activate (after my/yas-activate-global-mode ())
+;;   (let ((modes ad-return-value))
+;;     (setq ad-return-value (append modes (list 'global-mode)))))
 
-(ad-activate 'yas--modes-to-activate)
+;; (ad-activate 'yas--modes-to-activate)
+
+;; https://github.com/joaotavora/yasnippet/issues/557
+(add-hook 'yas-minor-mode-hook
+          (lambda ()
+            (yas-activate-extra-mode 'fundamental-mode)))
