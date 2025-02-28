5d7119e6ce7d6b258829f58cb675ae7e269442ca
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2025-02-28 15:09:23 -0700

    changes

--- emacs/my-dot-emacs.el
+++ emacs/my-dot-emacs.el
@@ -1,3 +1,8 @@
-(if (equal (read-char "Load all my customizations?" nil 2.0) ?y)
-    (load "~/my/dotfiles/emacs/my-dot-emacs-2")
-  (dired "~/my/dotfiles/emacs"))
+(let ((skip-customizations
+       (equal ?y
+              (ignore-errors
+                ;; Mouse clicks throws an error.
+                (read-char "Skip my customizations?" nil 2.0)))))
+  (if skip-customizations
+      (dired "~/my/dotfiles/emacs")
+    (load "~/my/dotfiles/emacs/my-dot-emacs-2")))
