35619f2731df369585597655b45ad38ec9b5fe45
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2025-03-14 05:11:41 -0700

    customize updates

--- emacs/my-dot-emacs.el
+++ emacs/my-dot-emacs.el
@@ -49,11 +49,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 170 :family "JetBrains Mono"))))
- ;; '(bm-persistent-face ((t (:background "#6e52b9" :foreground "#ffffff"))))
- ;; '(isearch ((t (:background "#ffffff" :foreground "#6e52b9"))))
- ;; '(lazy-highlight ((t (:background "#6e52b9" :foreground "#ffffff"))))
- )
+ '(default ((t (:height 170 :family "JetBrains Mono")))))
 
 (let ((skip-customizations
        (equal ?y
