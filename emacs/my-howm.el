45d0cf802f51f7e1f2c2a4abebb2a9924272ae81
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2025-03-23 23:15:31 -0700

    howm

---
+++ emacs/my-howm.el
@@ -0,0 +1,7 @@
+(require 'howm)
+
+(setq howm-file-name-format "%Y/%m/%Y-%m-%d-%H%M%S.org")
+
+;; Notes:
+;; https://leahneukirchen.org/blog/archive/2022/03/note-taking-in-emacs-with-howm.html
+;; https://news.ycombinator.com/item?id=41438107
