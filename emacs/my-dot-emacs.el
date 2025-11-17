9388ca901f331a05de35ae3f0fb9c4b631eddc66
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2025-11-17 09:10:03 -0700

    Ensure early-init.el link exists

--- emacs/my-dot-emacs.el
+++ emacs/my-dot-emacs.el
@@ -1,4 +1,18 @@
 ;; -*- lexical-binding: t; -*-
+
+(unless (boundp 'my/dotfiles-dir)
+  ;;; ChatGPT 5.1
+  (let* ((target (expand-file-name "~/my/dotfiles/emacs/my-early-init.el"))
+         (link   (expand-file-name "~/.emacs.d/early-init.el"))
+         (ok     (and (file-symlink-p link)
+                      (string-equal (file-truename link)
+                                    (file-truename target)))))
+  (unless ok
+    (when (file-exists-p link)
+      (delete-file link))
+    (make-symbolic-link target link t)
+    (restart-emacs))))
+
 (defvar my/load-print-messages nil)
 
 (defun my/load-full ()
