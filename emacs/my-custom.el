44c84d03f41bcc2f74e09b2de0866ca85e3a9a0e
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2025-11-18 17:35:25 -0700

    update

--- emacs/my-custom.el
+++ emacs/my-custom.el
@@ -49,5 +49,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 140 :family "JetBrains Mono"))))
- '(region ((t (:extend t :background "#283648" :foreground "#88A4B8")))))
+ '(default ((t (:height 140 :family "JetBrains Mono")))))
