ac50420ec014a3f8361fd6264a246e2d25ab2a58
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2026-03-18 19:08:38 -0700

    multi-select and open

--- emacs/my-embark.el
+++ emacs/my-embark.el
@@ -25,3 +25,54 @@
   (call-interactively 'switch-to-buffer))
 
 (define-key embark-buffer-map (kbd "o") 'my/embark-ace-action-switch-to-buffer)
+
+;; Helpers for Embark multi-file actions
+(defun my/embark--unique-files (files)
+  (let* ((files (if (listp files) files (list files)))
+         (files (mapcar (lambda (f) (expand-file-name f default-directory)) files)))
+    (delete-dups files)))
+
+(defun my/embark-open-files-in-new-tabs (files)
+  "Open FILES, one per new tab."
+  (interactive)
+  (let ((files (my/embark--unique-files files))
+        (orig-tab (1+ (tab-bar--current-tab-index (funcall tab-bar-tabs-function)))))
+    (message "%s" files)
+    (unless files (user-error "No files selected"))
+    (dolist (file files)
+      (tab-bar-new-tab)
+      (find-file file))
+    (tab-bar-select-tab (1+ orig-tab))))
+
+(defun my/embark-open-files-in-side-by-side-windows (files)
+  "Open FILES in current frame, one file per side-by-side window."
+  (interactive)
+  (let ((files (my/embark--unique-files files)))
+    (unless files (user-error "No files selected"))
+    (delete-other-windows)
+    (find-file (car files))
+    (dolist (file (cdr files))
+      (split-window-below)
+      (other-window 1)
+      (find-file file))
+    (balance-windows)))
+
+(with-eval-after-load 'embark
+  ;; No "Run ... on N files?" prompt
+  (setq embark-confirm-act-all nil)
+
+  ;; Make RET default to find-file for file candidates
+  (add-to-list 'embark-default-action-overrides '(file . find-file))
+
+  ;; Tell embark-act-all to pass the whole selected list to these commands
+  (dolist (fn '(my/embark-open-files-in-new-tabs
+                my/embark-open-files-in-side-by-side-windows))
+    (add-to-list 'embark-multitarget-actions fn))
+
+  ;; Actions in file action map
+  (bind-keys :map embark-file-map
+             ("T" . my/embark-open-files-in-new-tabs)
+             ("W" . my/embark-open-files-in-side-by-side-windows)))
+
+(bind-keys :map vertico-map
+           ("C-;" . embark-act-all))
