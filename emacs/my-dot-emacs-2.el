3fa5f156574284dd6385c0b54a2e1b577bc3e9dc
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2026-05-09 21:04:41 -0700

    org in now included in emacs and get latest from gnu elpa

--- emacs/my-dot-emacs-2.el
+++ emacs/my-dot-emacs-2.el
@@ -12,7 +12,6 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 
 
 (defvar my/colors
