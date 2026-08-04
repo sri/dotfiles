fda494a32d725b3c5edb7230af685e2895cb941e
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2026-08-03 17:01:57 -0700

    teminal upgrades

--- emacs/my-terminal.el
+++ emacs/my-terminal.el
@@ -48,3 +48,19 @@
 ;(face-spec-set 'hl-line '((t (:background "color-236" :inherit nil))))
 
 (xterm-mouse-mode)
+
+(dolist (key '([M-mouse-1]
+               [M-drag-mouse-1]
+               [M-down-mouse-1]
+               [M-mouse-2]
+               [M-mouse-3]))
+  (global-unset-key key))
+
+(setq select-enable-clipboard t
+      select-enable-primary nil)
+
+;; Enable Emacs's built-in OSC 52 clipboard support inside Herdr.
+(when (and (not (display-graphic-p))
+           (getenv "HERDR_PANE_ID"))
+  (require 'term/xterm)
+  (xterm--init-activate-set-selection))
