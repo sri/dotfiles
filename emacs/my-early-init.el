1825c797b478db90a3c57d71bc277a0fa953db46
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2026-08-05 12:26:31 -0700

    native comp causes some issues

--- emacs/my-early-init.el
+++ emacs/my-early-init.el
@@ -1,6 +1,24 @@
 ;; -*- lexical-binding: t; -*-
-(setq native-comp-speed -1)
-(setq native-comp-jit-compilation nil)
+
+;; Disable native compilation as early as possible.
+;; `package-native-compile' only affects package.el; the native-comp JIT can
+;; still compile loaded .elc files unless these are also disabled.
+(setq package-native-compile nil
+      native-comp-jit-compilation nil
+      native-comp-deferred-compilation nil
+      ;; Disable on-demand native compilation of trampolines for primitives
+      ;; like `sort'.  This can still happen even when package native-comp is
+      ;; disabled.
+      native-comp-enable-subr-trampolines nil
+      comp-enable-subr-trampolines nil
+      ;; Don't load previously generated native-code files from the eln cache.
+      ;; This is stricter than just disabling new native compilation.
+      native-comp-eln-load-path nil
+      ;; Even if something explicitly invokes native compilation, keep functions
+      ;; in bytecode form.  Emacs may still produce .eln files containing
+      ;; bytecode, but not native code.
+      native-comp-speed -1)
+
 (setq gc-cons-threshold most-positive-fixnum)
 (add-hook 'emacs-startup-hook
           (lambda () (setq gc-cons-threshold (* 50 1024 1024))))
