7ba155eab3da660050a48bb8aae21105214f4efc
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2026-08-07 22:51:05 -0700

    save

--- fish/config.fish
+++ fish/config.fish
@@ -1,38 +1,70 @@
 # -*- mode: fish -*-
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    # Commands to run in interactive sessions can go here.
 end
 
+############################################################
+# Shared with zsh/zshrc
+############################################################
 
 ### PATH
 set -U fish_user_paths ~/my/dotfiles/bin $fish_user_paths
 
-fish_add_path /opt/homebrew/bin
-
-
-
-### Aliases
-alias sub "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
-
 ### asdf
-# asdf has native fish support
 if test -f ~/.asdf/asdf.fish
     source ~/.asdf/asdf.fish
 end
 
 ### rbenv
-#if test -x ~/.rbenv/bin/rbenv
-#    status --is-interactive; and source (rbenv init - fish | psub)
-#end
-#
-### History (Fish already does most of this by default)
+if test -x ~/.rbenv/bin/rbenv
+    status --is-interactive; and source (~/.rbenv/bin/rbenv init - fish | psub)
+end
+
+### History
 set -U fish_history main
-set -g fish_history_limit 10000
 
 ### Environment variables
 set -x PYTHONSTARTUP ~/.pythonrc.py
 set -x RIPGREP_CONFIG_PATH ~/.ripgreprc
+
+### Prompt
+# set -g __fish_git_prompt_show_informative_status 0
+# set -g __fish_git_prompt_showdirtystate 0
+# set -g __fish_git_prompt_showupstream none
+# set -g __fish_git_prompt_showcolorhints 0
+
+# function fish_prompt
+#     set last_status $status
+#     set duration_s 0
+#     if set -q CMD_DURATION
+#         set duration_s (math --scale=0 "$CMD_DURATION / 1000")
+#     end
+
+#     set time_str (date +%H:%M:%S)
+#     set dir_str "["(prompt_pwd)"]"
+#     set branch_str (string trim -- (fish_git_prompt))
+#     set branch_segment ""
+#     if test -n "$branch_str"
+#         set branch_segment " $branch_str"
+#     end
+
+#     if set -q INSIDE_EMACS; or test "$TERM" = "dumb"
+#         if test $last_status -eq 0
+#             set prompt_status "OK"
+#         else
+#             set prompt_status "ERR($last_status)"
+#         end
+#         printf '%s %s%s {%ss:%s}\n> ' $time_str $dir_str $branch_segment $duration_s $prompt_status
+#     else
+#         if test $last_status -eq 0
+#             set status_symbol (set_color green)"✔"(set_color normal)
+#         else
+#             set status_symbol (set_color red)"✘"(set_color normal)
+#         end
+#         printf '%s %s%s {%ss:%s}\n→ ' (set_color cyan)$time_str(set_color normal) (set_color magenta)$dir_str(set_color normal) $branch_segment $duration_s $status_symbol
+#     end
+# end
 
 ### Cargo
 if test -f ~/.cargo/env.fish
@@ -40,9 +72,10 @@ if test -f ~/.cargo/env.fish
 end
 
 ############################################################
-# Aliases & Functions
+# Aliases & functions shared with zsh/zshrc
 ############################################################
 
+alias sub "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
 alias tab 'open . -a iterm'
 
 alias d 'git diff'
@@ -97,7 +130,7 @@ alias cddotfiles 'c ~/my/dotfiles'
 alias x 'chmod +x'
 
 alias rr 'source ~/.config/fish/config.fish'
-alias brc 'emacsclient ~/.zshrc; source ~/.config/fish/config.fish'
+alias brc 'emacsclient ~/.config/fish/config.fish; source ~/.config/fish/config.fish'
 
 alias df 'df -h'
 alias du 'du -c -h'
@@ -105,10 +138,8 @@ alias du1 'du --max-depth=1'
 
 alias topcmds 'cat ~/.local/share/fish/fish_history | grep -oE "cmd=.*" | sed "s/cmd=//" | sort | uniq -c | sort -nr | head -15'
 
-
-# ls
-alias ll 'eza --long --sort modified --total-size --almost-all --group-directories-first'
-alias llsz 'eza --long --sort size --total-size --almost-all --group-directories-first'
+alias llsz`1234 'eza --long --sort modified --almost-all --group-directories-first'
+alias llsz 'eza --long --sort size --almost-all --group-directories-first'
 alias llt 'eza --tree'
 
 alias curl_info 'curl -so /dev/null -w "dnslookup: %{time_namelookup} | connect: %{time_connect} | appconnect: %{time_appconnect} | pretransfer: %{time_pretransfer} | starttransfer: %{time_starttransfer} | total: %{time_total} | size: %{size_download}\n"'
@@ -149,9 +180,9 @@ alias l my_ls
 function my_ls
     if test (count $argv) -eq 1; and test -f $argv[1]
         less $argv[1]
-    else
-        ll $argv
+        return
     end
+    ll $argv
 end
 
 alias c cd_ls
@@ -162,6 +193,16 @@ function cd_ls
         cd $argv; and ls -G
     end
 end
+
+if test -f ~/.config/fish/config_private.fish
+    source ~/.config/fish/config_private.fish
+end
+
+############################################################
+# Fish-only extras (missing in zsh/zshrc)
+############################################################
+
+fish_add_path /opt/homebrew/bin
 
 function e
     ~/progs/emacs/bin/emacs &
@@ -184,4 +225,7 @@ end
 if type -q oh-my-posh
     oh-my-posh init fish | source
 end
-/Users/sri/.local/bin/mise activate fish | source
+
+if test -x /Users/sri/.local/bin/mise
+    /Users/sri/.local/bin/mise activate fish | source
+end
