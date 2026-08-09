4073f3a61ff74dfe5beab30ac1b661d1143676ea
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2026-08-09 03:38:40 -0700

    have copy selection work under all terminals

--- emacs/my-terminal.el
+++ emacs/my-terminal.el
@@ -59,8 +59,17 @@
 (setq select-enable-clipboard t
       select-enable-primary nil)
 
-;; Enable Emacs's built-in OSC 52 clipboard support inside Herdr.
-(when (and (not (display-graphic-p))
-           (getenv "HERDR_PANE_ID"))
-  (require 'term/xterm)
-  (xterm--init-activate-set-selection))
+;; Enable Emacs's built-in OSC 52 clipboard support for every TTY.  This must
+;; run per terminal: when Emacs is a daemon, doing it only during startup sets
+;; the parameter on `initial_terminal`, not on later emacsclient frames.
+(require 'term/xterm)
+
+(defun my/enable-tty-osc52-clipboard ()
+  (unless (display-graphic-p)
+    (xterm--init-activate-set-selection)))
+
+(add-hook 'tty-setup-hook #'my/enable-tty-osc52-clipboard)
+
+;; Also fix terminal frames that already exist when this file is reloaded.
+(dolist (terminal (terminal-list))
+  (set-terminal-parameter terminal 'xterm--set-selection t))
