1dff63bc6e16b4dafa17bbfe51c403d98dfdd49b
Sriram Thaiyar <sriram.thaiyar@gmail.com>
2026-08-26 22:46:59 -0700

    fix package-reinstall

--- emacs/my-early-init.el
+++ emacs/my-early-init.el
@@ -1,4 +1,8 @@
 ;; -*- lexical-binding: t; -*-
+(defvar my/disabled-eln-cache
+  (expand-file-name "disabled-eln-cache/" user-emacs-directory))
+
+(make-directory my/disabled-eln-cache t)
 
 ;; Disable native compilation as early as possible.
 ;; `package-native-compile' only affects package.el; the native-comp JIT can
@@ -13,7 +17,7 @@
       comp-enable-subr-trampolines nil
       ;; Don't load previously generated native-code files from the eln cache.
       ;; This is stricter than just disabling new native compilation.
-      native-comp-eln-load-path nil
+      native-comp-eln-load-path (list my/disabled-eln-cache)
       ;; Even if something explicitly invokes native compilation, keep functions
       ;; in bytecode form.  Emacs may still produce .eln files containing
       ;; bytecode, but not native code.
